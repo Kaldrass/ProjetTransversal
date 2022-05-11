18fb9400aa0bc31c17bdeae94570a9bb81d53391
timothee-l <tim.levilly@gmail.com>
2022-05-11 09:05:44 +0200

    ajouts multiprocessing

--- comm/testsParallelisme/communication.py
+++ comm/testsParallelisme/communication.py
@@ -3,11 +3,11 @@ Gestionnaires de communication
 - STM32 :
     Lecture et écriture en alternance
     Lecture :
-        Par paquet de 8 octet jusqu'à recevoir un octet contenant l'information de fin de transmission
-    Ecriture :
+        Par paquet de 8 octets jusqu'à recevoir un octet contenant l'information de fin de transmission
+    Écriture :
         Comme établi avant (rotation / translation + sens + valeur) à partir de la seconde queue
 - LIDAR :
     Réception uniquement
-
+- Caméra
 Envoi des données dans la Queue avec Header indiquant le type de capteur
 """
--- comm/testsParallelisme/traitement.py
+++ comm/testsParallelisme/traitement.py
@@ -2,8 +2,21 @@
 Démultiplexage pour traiter chaque type de données indépendamment
 Traitement données
 Utilisation du résultat pour construire et compléter le graph (!) / Pour identifier notre position dans le graphe
+Capteurs : roue codeuse, caméra, LIDAR
 """
 
 
-def traitement():
+def inRoueCodeuse():
+    print("ok")
+
+
+def inCamera():
+    print("ok")
+
+
+def inLidar():
+    print("ok")
+
+
+def inUltrason():
     print("ok")
