2329a66309d7b02550425990d95ba8ca6e68fba4
timothee-l <tim.levilly@gmail.com>
2022-05-18 09:35:22 +0200

    merge melissa

--- comm/testsParallelisme/socketReception.py
+++ comm/testsParallelisme/socketReception.py
@@ -1,3 +1,17 @@
 """
 Réceptionne les instructions transmises par le serveur web pour les ajouter à la Queue d'instructions
 """
+#On recoit des octets on les décode
+import socket
+import multiprocessing as mp
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(),8000))
+
+while True:
+    msg = s.resc(1024) # exemple buffer how big is the data qu'on veut receive
+    if len(msg) <= 0:
+        break
+    else:
+        print(msg.decode("utf-8"))
+
--- server.py
+++ server.py
@@ -1,3 +1,12 @@
 import django
-
+import socket
 print(django.get_version())
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(,8000))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"connection from {address} has been established!")
+    clientsocket.send()#mettre ce qu'on veut envoyer au client exemple "blabla",utf-8.
